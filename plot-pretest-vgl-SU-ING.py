b43b06c4a142d096a34852c3fa7ac250e9b3e705
Alexander Voigt <alexander.voigt@physik.rwth-aachen.de>
2024-02-13 11:51:02 +0100

    adding plot to compare SU and ING in pretest

--- plot-pretest-vgl-SU-ING.py
+++ plot-pretest-vgl-SU-ING.py
@@ -5,43 +5,35 @@ import numpy as np
 import pandas as pd
 import rounding
 import style
-import sys
 
-if len(sys.argv) != 2:
-    raise "Error: 1 argument expected"
-
-filename_prefix = sys.argv[1]
-
-print(f'filename_prefix = {filename_prefix}')
-
-# rm -rf ~/.cache/matplotlib/
-# from matplotlib import font_manager
-# font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
-# font_manager.findfont("Atkinson Hyperlegible")
+filename_prefix_SU = 'pretest-sachunterricht'
+filename_prefix_ING = 'pretest-ingenieure'
 
 plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['font.sans-serif'] = 'Atkinson Hyperlegible'
 
-bar_colors    = {'ges': style.my_blue  , 'int': 'w'            , 'kon': 'w'             }
-bar_fill      = {'ges': True           , 'int': True           , 'kon': True            }
-bar_hatch     = {'ges': None           , 'int': style.my_hatch1, 'kon': style.my_hatch2 }
-bar_edgecolor = {'ges': style.my_blue  , 'int': style.my_red   , 'kon': style.my_green  }
-bar_textcolor = {'ges': 'w'            , 'int': 'k'            , 'kon': 'k'             }
-bar_errcolor  = {'ges': style.my_orange, 'int': 'k'            , 'kon': 'k'             }
+bar_colors    = {'SU': 'w'            , 'ING': 'w'             }
+bar_fill      = {'SU': True           , 'ING': True            }
+bar_hatch     = {'SU': style.my_hatch3, 'ING': style.my_hatch4 }
+bar_edgecolor = {'SU': style.my_blue  , 'ING': style.my_red    }
+bar_textcolor = {'SU': 'w'            , 'ING': 'k'             }
+bar_errcolor  = {'SU': 'k'            , 'ING': 'k'             }
 
 # daten einlesen
-data = pd.read_csv(filename_prefix + '.csv', delimiter=';')
+data_SU  = pd.read_csv(filename_prefix_SU  + '.csv', delimiter=';')
+data_ING = pd.read_csv(filename_prefix_ING + '.csv', delimiter=';')
+data_all = { 'SU': data_SU, 'ING': data_ING }
 
-for g in daten.gruppen:
+for d in data_all:
+    g = 'ges'
+    data = data_all[d]
+    data.set_index('Konzept', inplace=True)
     # Standard-Error
     data['se'  + g] = data['sd' + g]/np.sqrt(data['N' + g])
     # relativer Punkteanteil 
     data['r'   + g] = data['m'  + g]/data['PunkteMax']
     # relativer Standard-Error
     data['rse' + g] = data['se' + g]/data['PunkteMax']
-
-data.set_index('Konzept', inplace=True)
-print(data)
 
 label_locations = np.arange(len(daten.konzepte))
 width = style.width
@@ -52,18 +44,19 @@ ax.grid(axis='y', linestyle='-', linewidth=1)
 ax.set_axisbelow(True)
 
 # Balken
-for g in daten.gruppen:
+for d in data_all:
+    g = 'ges'
+    data = data_all[d]
     dat = data['r' + g]*100
     err = data['rse' + g]*100
-    # print(dat)
     offset = width * multiplier
     x = label_locations + offset
     rects = ax.bar(x, dat, width,
-                   label=data[g].loc['ogSK'],
-                   color=bar_colors[g],
-                   fill=bar_fill[g],
-                   hatch=bar_hatch[g],
-                   edgecolor=bar_edgecolor[g])
+                   label=d,
+                   color=bar_colors[d],
+                   fill=bar_fill[d],
+                   hatch=bar_hatch[d],
+                   edgecolor=bar_edgecolor[d])
     ax.bar_label(rects,
                  labels=[f'{r:2.0f}%' for r in [rounding.round_half_up(d) for d in dat]],
                  label_type='center',
@@ -71,28 +64,30 @@ for g in daten.gruppen:
                  backgroundcolor='white',
                  fontsize='small')
     ax.errorbar(x, dat, yerr=err, fmt=' ',
-                color=bar_errcolor[g],
+                color=bar_errcolor[d],
                 linewidth=2,
                 capthick=2,
                 capsize=3)
     multiplier += 1
 
+significance = ['sig.', 'sig.', 'n.s.', 'sig.', 'sig.']
+
 # geschweifte Klammern
-for xybalken in zip(label_locations + 1.5*width, data['rint']*100 + 7, data['sig']):
+for xybalken in zip(label_locations + 0.5*width, data_all['ING']['rges']*100 + 7, significance):
     ax.annotate(xybalken[2], xy=(xybalken[0], xybalken[1]), xytext=(xybalken[0], xybalken[1]+5),
                 ha='center', va='bottom',
                 arrowprops=dict(arrowstyle='-[, widthB=2.5, lengthB=0.75', lw=2))
 
 ax.set_ylabel('erreichte Punktzahl in Prozent')
-# ax.set_title('Studierende ' + data['studiengang'].loc['ogSK'] + ' (Pretest)')
-ax.set_xticks(label_locations + width, daten.konzepte_label)
+ax.set_xticks(label_locations + width/2, daten.konzepte_label)
 ax.yaxis.set_major_formatter('{x:.0f}%')
 ax.yaxis.set_major_locator(plticker.MultipleLocator(base=10))
 legend = ax.legend(loc='upper right', ncol=1, edgecolor='k')
 legend.get_frame().set_alpha(None)
-ax.annotate(r'$\alpha=0{,}01$', xy=(4.9,73), ha='right', va='center')
+ax.annotate(r'$\alpha=0{,}01$', xy=(4.6,80), ha='right', va='center')
 ax.set_ylim(0, 100)
 
+filename_prefix = 'pretest-vgl-SU-ING'
 fig.tight_layout()
 fig.savefig(filename_prefix + '.pdf')
 fig.savefig(filename_prefix + '.png', dpi=300)
--- style.py
+++ style.py
@@ -4,6 +4,10 @@ my_green  = '#090'
 my_orange = '#e80'
 my_hatch1 = '////'
 my_hatch2 = '\\\\\\\\'
+my_hatch3 = '||||'
+my_hatch4 = '----'
 
 cm = 1/2.54
 my_figsize = (20*cm, 10*cm)
+
+width = 0.28
